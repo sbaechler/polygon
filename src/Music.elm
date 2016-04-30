6e9a2ea7814d72f0313634c0232f645e9f25a06e
Simon Bächler <simon@stellanera.com>
2016-04-30 18:42:42 +0200

    add timecodes

---
+++ src/Music.elm
@@ -0,0 +1,10 @@
+module Music where
+
+bassSwitch : List Int -- ms
+bassSwitch = [14760, 44313, 51668, 129193, 14387]
+
+-- possible additional entry points
+entryPoints = [84718]
+
+-- point where to loop
+loopPoint = 188331
